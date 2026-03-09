73e22182ca1b8dd7626c21eddc1f88309c2664b4
Junxian Ma <edgep@JunxiandeMacBook-Air.local>
2026-03-09 15:19:23 +0800

    fix: update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://edgep.github.io",
+  site: "https://edgepro001.github.io",
   output: "static",
 });
